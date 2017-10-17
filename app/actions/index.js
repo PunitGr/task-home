6365259dd6fa3d48190c2bbaad40163ee424e9cd
PunitGr <groove679@gmail.com>
2017-10-18 02:21:48 +0530

    Add actions for editMember and showStatus

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,13 +1,17 @@
 // @flow
 import {
     ADD_MEMBER,
-    DELETE_MEMBER
+    DELETE_MEMBER,
+    SHOW_STATUS,
+    EDIT_MEMBER
 } from "../constants";
 
 export type Action = {
     type: string,
     id?: number,
-    value?: Object
+    value?: Object,
+    show?: boolean,
+    editId?: number
 };
 
 export function addMember(id: number, value: Object): Action {
@@ -22,5 +26,19 @@ export function deleteMember(id: number): Action {
     return {
         type: DELETE_MEMBER,
         id
+    };
+}
+
+export function showStatus(show: boolean): Action {
+    return {
+        type: SHOW_STATUS,
+        show
+    };
+}
+
+export function editMember(editId: number): Action {
+    return {
+        type: EDIT_MEMBER,
+        editId
     };
 }
--- app/constants/index.js
+++ app/constants/index.js
@@ -2,3 +2,5 @@
 
 export const ADD_MEMBER: string = "ADD_MEMBER";
 export const DELETE_MEMBER: string = "DELETE_MEMBER";
+export const SHOW_STATUS: string = "SHOW_STATUS";
+export const EDIT_MEMBER: string = "EDIT_MEMBER";
